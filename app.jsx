596627880eed9dcfbc2ee9abb7f40bcace5c7349
Pedro2883-lab <pedro.lebid.santos@escola.pr.gov.br>
2025-08-25 08:41:22 -0300

    oi

--- app.jsx
+++ app.jsx
@@ -1,1 +1,24 @@
+import React, {useState, useEffect} from 'react';
+import {view, Stylesheet, image} from 'react-native';
 
+export default function app (){
+    return(
+        <View style={Stylesheet.container}>
+            <image 
+            //caminho da imagem
+             source={requestAnimationFrame('./assets/spotify-logo.png')}
+             style={Stylesheet.logo}
+                resideMode={contain
+            acessible
+            acessiblebilityLabel = "Spotify"
+
+                
+              )
+}
+
+const styles = Stylesheet.create({
+    container:{
+        flex: 1;
+        backgroundcolor:'#000'
+    }
+})
